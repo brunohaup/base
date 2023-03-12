7e468c546af76f683bba82f20aca0a8588444405
Bruno <Bruno@DESKTOP-VIGHU92.local>
2023-03-11 23:30:21 -0300

    primeiro commit

--- src/main/java/base/BaseApplication.java
+++ src/main/java/base/BaseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaseApplication.class, args);
+		SpringApplication.run(BaseApplication.class, args); 
 	}
 
 }
